122cb87d3f889502090909c83b40c80b22c148ca
Akhil Gorantala <akhilgorantala1@gmail.com>
2025-09-19 08:47:33 -0400

    initial push

---
+++ src/linkedlist/Main.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class Main {
+    public static void main(String[] args){
+        LinkedList myLinkedList = new LinkedList(11);
+        myLinkedList.append(3);
+        myLinkedList.append(23);
+        myLinkedList.append(7);
+
+        myLinkedList.set(1, 4);
+
+        myLinkedList.printList();
+    }
+}
