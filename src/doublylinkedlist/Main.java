ee65a94bbeed5c5f73ed23f244b1389b74945b62
Akhil Gorantala <akhilgorantala1@gmail.com>
2025-09-22 07:46:58 -0400

    doubly linked list added

---
+++ src/doublylinkedlist/Main.java
@@ -0,0 +1,13 @@
+package doublylinkedlist;
+
+public class Main {
+    public static void main(String[] args){
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
+        myDLL.append(3);
+
+        myDLL.remove(1);
+
+        myDLL.printList();
+    }
+}
