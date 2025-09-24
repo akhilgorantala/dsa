ec5ce4916196df216e7af5cd12712f26ab02df43
Akhil Gorantala <akhilgorantala1@gmail.com>
2025-09-24 12:35:38 -0400

    queue added

---
+++ src/queue/Main.java
@@ -0,0 +1,13 @@
+package queue;
+
+public class Main {
+    public static void main(String[] args){
+        Queue myQueue = new Queue(1);
+
+        myQueue.enqueue(2);
+
+        myQueue.dequeue();
+
+        myQueue.printQueue();
+    }
+}
